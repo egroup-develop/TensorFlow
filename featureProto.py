00822fd8d515b0145e13deb2390f01a71d191d80
camberbridge <camberbridgecircuit@gmail.com>
2015-11-29 19:54:43 +0900

    2層NNでの分類. 実行すると, 各ステップでの正解率を表示する

--- featureProto.py
+++ featureProto.py
@@ -1,3 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import cv2
+import numpy as np
+import tensorflow as tf
+import tensorflow.python.platform
+
+#多値分類ということで
+NUM_CLASSES = 100
+IMAGE_SIZE = 28
+IMAGE_PIXELS = IMAGE_SIZE*IMAGE_SIZE*3
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+flags.DEFINE_string('train', 'train.txt', 'File name of train data')
+flags.DEFINE_string('test', 'test.txt', 'File name of train data')
+#tensorboardに出力するファイルの置き場
+flags.DEFINE_string('train_dir', '/tmp/data', 'Directory to put the training data.')
+flags.DEFINE_integer('max_steps', 200, 'Number of steps to run trainer.')
+flags.DEFINE_integer('batch_size', 10, 'Batch size'
+                     'Must divide evenly into the dataset sizes.')
+flags.DEFINE_float('learning_rate', 1e-4, 'Initial learning rate.')
+
 ################ interface ####################
 # 入力: 28x28x3(縦画素x横画素x枚数)
 # 畳み込み: 2回
@@ -117,3 +141,128 @@ def training(loss, learning_rate):
     #AdamOptimizerは全体のネットを最適化(自動微分のような)してくれる便利な関数
     train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss)
     return train_step
+
+############################ 正解率の計算 ######################
+# そういうこと
+################################################################
+def accuracy(logits, labels):
+    """ 正解率(accuracy)を計算する関数
+
+    引数: 
+      logits: inference()の結果
+      labels: ラベルのtensor, int32 - [batch_size, NUM_CLASSES]
+
+    返り値:
+      accuracy: 正解率(float)
+
+    """
+    correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(labels, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    tf.scalar_summary("accuracy", accuracy)
+    return accuracy
+
+############### データセットの読み込み・学習の実行 #############
+# TensorFlowにはdecode_pngのような画像を読み込むための関数が用意されている
+# http://www.tensorflow.org/api_docs/python/image.html#decode_png
+# 今回はこれを使わずcv2で読み込む
+################################################################
+if __name__ == '__main__':
+    # ファイルを開く
+    f = open(FLAGS.train, 'r')
+    # データを入れる配列
+    train_image = []
+    train_label = []
+    for line in f:
+        # 文字列の末尾(改行)を除いてスペース区切りにする
+        line = line.rstrip()
+        l = line.split()
+        # データを読み込んで28x28に縮小
+        img = cv2.imread(l[0])
+        img = cv2.resize(img, (28, 28))
+        # 一列にした後、0-1のfloat値にする
+        train_image.append(img.flatten().astype(np.float32)/255.0)
+        # ラベルを1-of-k方式で用意する
+        tmp = np.zeros(NUM_CLASSES)
+        tmp[int(l[1])] = 1
+        train_label.append(tmp)
+    # numpy形式に変換
+    train_image = np.asarray(train_image)
+    train_label = np.asarray(train_label)
+    f.close()
+
+    f = open(FLAGS.test, 'r')
+    test_image = []
+    test_label = []
+    for line in f:
+        line = line.rstrip()
+        l = line.split()
+        img = cv2.imread(l[0])
+        img = cv2.resize(img, (28, 28))
+        test_image.append(img.flatten().astype(np.float32)/255.0)
+        tmp = np.zeros(NUM_CLASSES)
+        tmp[int(l[1])] = 1
+        test_label.append(tmp)
+    test_image = np.asarray(test_image)
+    test_label = np.asarray(test_label)
+    f.close()
+    
+    with tf.Graph().as_default():
+        # 画像を入れる仮のTensor
+        images_placeholder = tf.placeholder("float", shape=(None, IMAGE_PIXELS))
+        # ラベルを入れる仮のTensor
+        labels_placeholder = tf.placeholder("float", shape=(None, NUM_CLASSES))
+        # dropout率を入れる仮のTensor
+        keep_prob = tf.placeholder("float")
+
+        # inference()を呼び出してモデルを作る
+        logits = inference(images_placeholder, keep_prob)
+        # loss()を呼び出して損失を計算
+        loss_value = loss(logits, labels_placeholder)
+        # training()を呼び出して訓練
+        train_op = training(loss_value, FLAGS.learning_rate)
+        # 精度の計算
+        acc = accuracy(logits, labels_placeholder)
+
+        # 保存の準備
+        saver = tf.train.Saver()
+        # Sessionの作成
+        sess = tf.Session()
+        # 変数の初期化
+        sess.run(tf.initialize_all_variables())
+        # TensorBoardで表示する値の設定
+        summary_op = tf.merge_all_summaries()
+        summary_writer = tf.train.SummaryWriter(FLAGS.train_dir, sess.graph_def)
+        
+        # 訓練の実行
+        for step in range(FLAGS.max_steps):
+            for i in range(len(train_image)/FLAGS.batch_size):
+                # batch_size分の画像に対して訓練の実行
+                batch = FLAGS.batch_size*i
+                # feed_dictでplaceholderに入れるデータを指定する
+                sess.run(train_op, feed_dict={
+                  images_placeholder: train_image[batch:batch+FLAGS.batch_size],
+                  labels_placeholder: train_label[batch:batch+FLAGS.batch_size],
+                  keep_prob: 0.5})
+
+            # 1 step終わるたびに精度を計算する
+            train_accuracy = sess.run(acc, feed_dict={
+                images_placeholder: train_image,
+                labels_placeholder: train_label,
+                keep_prob: 1.0})
+            print "step %d, training accuracy %g"%(step, train_accuracy)
+
+            # 1 step終わるたびにTensorBoardに表示する値を追加する
+            summary_str = sess.run(summary_op, feed_dict={
+                images_placeholder: train_image,
+                labels_placeholder: train_label,
+                keep_prob: 1.0})
+            summary_writer.add_summary(summary_str, step)
+
+    # 訓練が終了したらテストデータに対する精度を表示
+    print "test accuracy %g"%sess.run(acc, feed_dict={
+        images_placeholder: test_image,
+        labels_placeholder: test_label,
+        keep_prob: 1.0})
+
+    # 最終的なモデルを保存
+    save_path = saver.save(sess, "model.ckpt")
