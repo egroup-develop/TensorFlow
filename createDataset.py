85f4c3e3725df2fa7149825fe3b9e8c806d29175
camberbridge <camberbridgecircuit@gmail.com>
2015-11-29 19:00:48 +0900

    訓練データと評価データを作るようにした.数は3:1で. 本当は9:1くらいにしたい

--- createDataset.py
+++ createDataset.py
@@ -2,23 +2,37 @@
 import os
 import sys
 
-title = "train.txt"
+trainTitle = "train.txt"
+testTitle = "test.txt"
 
 #データセットテキストファイルに変換したい2階層ディレクトリを引数に与える
 os.chdir(sys.argv[1])
 
 for i in range(0, 100):
-  textCode = ""
+  trainText = ""
+  testText = ""
   dirName = str(i)
 
   files = os.listdir(dirName)
 
+  j = 0
   for file in files:
-    textCode += sys.argv[1] + dirName + "/" + file + " " + dirName + "\n"
+    if j < len(files) - 1:
+      trainText += sys.argv[1] + dirName + "/" + file + " " + dirName + "\n"
+    else:
+      testText += sys.argv[1] + dirName + "/" + file + " " + dirName + "\n"
+    j += 1
   
   try:   
-    f = open(title, "a+")
-    f.write(textCode)
+    f = open(trainTitle, "a+")
+    f.write(trainText)
+    f.close()
+  except:
+    sys.exit()
+
+  try:   
+    f = open(testTitle, "a+")
+    f.write(testText)
     f.close()
   except:
     sys.exit()
