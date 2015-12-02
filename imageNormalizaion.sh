3ff338cd54df528f0279b080a7006da73dd1bc25
camberbridge <camberbridgecircuit@gmail.com>
2015-12-02 18:26:20 +0900

    画像を28x28に正規化するシェル

--- imageNormalizaion.sh
+++ imageNormalizaion.sh
@@ -16,7 +16,6 @@ do
 
         for f in "$d"/*
         do
-                #python ./faceCutOut.py ${f} 2> /dev/null
                 convert $f -resize x28 -resize '28x<' -gravity center -crop 28x28+0+0 +repage ${f%.*}_convert.jpeg
         done
 done
