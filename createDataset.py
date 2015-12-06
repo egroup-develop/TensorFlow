ba27b4990e175642ea8d485f00bcf59ddf4d2c6a
camberbridge <camberbridgecircuit@gmail.com>
2015-12-06 19:25:32 +0900

    データセットを正規化した. 数も増やした

--- createDataset.py
+++ createDataset.py
@@ -8,20 +8,30 @@ testTitle = "test.txt"
 #データセットテキストファイルに変換したい2階層ディレクトリを引数に与える
 os.chdir(sys.argv[1])
 
-for i in range(0, 100):
+for i in range(0, 245):
   trainText = ""
   testText = ""
   dirName = str(i)
 
   files = os.listdir(dirName)
 
-  j = 0
+#  j = 0
+  eval = 0
   for file in files:
-    if j < len(files) - 1:
-      trainText += sys.argv[1] + dirName + "/" + file + " " + dirName + "\n"
-    else:
+    if file == "image_4_convert.jpeg" or file == "image_4_origin_convert.jpeg":
       testText += sys.argv[1] + dirName + "/" + file + " " + dirName + "\n"
-    j += 1
+      eval = 1
+    elif file.endswith("convert.jpeg", 15, len(file)) or file.endswith("convert.jpeg", 8, len(file)):
+      if eval == 0:
+        trainText += sys.argv[1] + dirName + "/" + file + " " + dirName + "\n"
+    eval = 0
+
+#    if j < len(files) - 1:
+#      trainText += sys.argv[1] + dirName + "/" + file + " " + dirName + "\n"
+#    else:
+#      testText += sys.argv[1] + dirName + "/" + file + " " + dirName + "\n"
+#    j += 1
+  
   
   try:   
     f = open(trainTitle, "a+")
