822ae32430ab170d3050a838860b4d61a4b72ff0
camberbridge <camberbridgecircuit@gmail.com>
2015-12-13 17:21:04 +0900

    ソース画像を10度ずつ左回転させる

---
+++ imageRotate.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+ 
+import cv2
+import numpy as np
+import sys
+from PIL import Image
+
+if __name__ == '__main__': 
+  img_path = sys.argv[1]
+  angle = 0
+
+  img_src = Image.open(img_path)
+  img_path = img_path.replace(".jpeg", "")
+
+  i = 10
+  while i <= 360:
+    angle = i
+    tmp = img_src.rotate(angle)
+    tmp.save(img_path + "_" + str(angle) + "rotated.jpeg")
+    i += 10
+
+
+##以下opencvでの画像回転
+#  # 画像読み込み
+#  img_src = cv2.imread(img_path, 1)
+
+#  # 画像の中心位置
+#  # 今回は画像サイズの中心をとっている
+#  center = tuple(np.array([img_src.shape[1] * 0.5, img_src.shape[0] + 0.5]))
+
+#  # 画像サイズの取得(横, 縦)
+#  size = tuple(np.array([img_src.shape[1], img_src.shape[0]]))
+
+#  # 回転させたい角度
+#  # ラジアンではなく角度(°)
+#  angle = 90.0
+
+#  # 拡大比率
+#  scale = 1.0
+
+#  # 回転変換行列の算出
+#  rotation_matrix = cv2.getRotationMatrix2D(center, angle, scale)
+
+#  # アフィン変換
+#  img_rot = cv2.warpAffine(img_src, rotation_matrix, size, flags=cv2.INTER_CUBIC)
+
+#  img_path = img_path.replace(".jpeg", "")
+#  # 保存
+#  cv2.imwrite(img_path + str(angle) + "_rotated.jpeg", img_rot)
